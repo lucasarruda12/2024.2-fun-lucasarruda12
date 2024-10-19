7edcacee60eefa9f85d8cc37a7f6d52fd521a712
LucasArruda12 <lucas123manoel4@gmail.com>
2024-10-19 11:53:50 -0300

    ExNat (- lo)

--- hw/ExNat.hs
+++ hw/ExNat.hs
@@ -34,23 +34,37 @@ instance Show Nat where
 
     -- zero  should be shown as O
     -- three should be shown as SSSO
-    show = undefined
+    show O      = "O"
+    show (S n)  = "S" ++ show n
 
 instance Eq Nat where
 
-    (==) = undefined
+    O     == O      = True
+    (S n) == (S m)  = n == m
+    _     == _      = False
 
 instance Ord Nat where
 
-    (<=) = undefined
+    O     <= _      = True
+    _     <= O      = False
+    (S n) <= (S m)  = n <= m
+    
 
     -- Ord does not REQUIRE defining min and max.
     -- Howevener, you should define them WITHOUT using (<=).
     -- Both are binary functions: max m n = ..., etc.
 
-    min = undefined
+    min O _ = O
+    min _ O = O
+    min (S n) (S m)
+      | n <= m    = S n
+      | otherwise = S m
 
-    max = undefined
+    max O n = n
+    max n O = n
+    max (S n) (S m)
+      | n <= m    = S m
+      | otherwise = S n
 
 
 ----------------------------------------------------------------
@@ -58,17 +72,23 @@ instance Ord Nat where
 ----------------------------------------------------------------
 
 isZero :: Nat -> Bool
-isZero = undefined
+isZero O = True
+isZero _ = False
 
 -- pred is the predecessor but we define zero's to be zero
 pred :: Nat -> Nat
-pred = undefined
+pred O      = O
+pred (S n)  = n
 
 even :: Nat -> Bool
-even = undefined
+even O          = True
+even (S O)      = False
+even (S (S n))  = even n
 
 odd :: Nat -> Bool
-odd = undefined
+odd O          = False
+odd (S O)      = True
+odd (S (S n))  = even n
 
 
 ----------------------------------------------------------------
@@ -77,34 +97,47 @@ odd = undefined
 
 -- addition
 (<+>) :: Nat -> Nat -> Nat
-(<+>) = undefined
+n <+> O     = n
+n <+> (S m) = S(n <+> m)
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
 -- It behaves like subtraction, except that it returns 0
 -- when "normal" subtraction would return a negative number.
 (<->) :: Nat -> Nat -> Nat
-(<->) = undefined
+O <-> n          = O
+n <-> O          = n
+(S n) <-> (S m)  = n <-> m
 
 -- multiplication
 (<*>) :: Nat -> Nat -> Nat
-(<*>) = undefined
+n <*> O     = O
+n <*> (S m) = n + (n <*> m)
 
 -- exponentiation
 (<^>) :: Nat -> Nat -> Nat
-(<^>) = undefined
+n <^> O     = S O
+n <^> (S m) = n * (n <^> m)
 
 -- quotient
 (</>) :: Nat -> Nat -> Nat
-(</>) = undefined
+n </> O = error "division by zero"
+n </> m 
+  | n == m    = S O
+  | n <= m    = O
+  | otherwise = n </> (n <-> m)
 
 -- remainder
 (<%>) :: Nat -> Nat -> Nat
-(<%>) = undefined
+n <%> O = error "division by zero"
+n <%> m 
+  | n == m    = O
+  | n <= m    = n
+  | otherwise = n </> (n <-> m)
 
 -- divides
 (<|>) :: Nat -> Nat -> Bool
-(<|>) = undefined
+n <|> m = isZero (n <%> m)
 
 divides = (<|>)
 
@@ -112,16 +145,20 @@ divides = (<|>)
 -- x `absDiff` y = |x - y|
 -- (Careful here: this - is the real minus operator!)
 absDiff :: Nat -> Nat -> Nat
-absDiff = undefined
+absDiff n m
+  | n <= m    = m <-> n
+  | otherwise = n <-> m
 
 (|-|) = absDiff
 
 factorial :: Nat -> Nat
-factorial = undefined
+factorial O     = S O
+factorial (S n) = S n <*> factorial n
 
 -- signum of a number (-1, 0, or 1)
 sg :: Nat -> Nat
-sg = undefined
+sg O = 0
+sg _ = 1
 
 -- lo b a is the floor of the logarithm base b of a
 lo :: Nat -> Nat -> Nat
@@ -136,11 +173,14 @@ lo = undefined
 -- Do NOT use the following functions in the definitions above!
 
 toNat :: Integral a => a -> Nat
-toNat = undefined
+toNat x
+  | x == 0    = O
+  | x > 0     = S (toNat (x-1))
+  | otherwise = toNat (- x)
 
 fromNat :: Integral a => Nat -> a
-fromNat = undefined
-
+fromNat O     = 0
+fromNat (S n) = 1 + fromNat(n)
 
 -- Voilá: we can now easily make Nat an instance of Num.
 instance Num Nat where
@@ -151,7 +191,7 @@ instance Num Nat where
     abs n = n
     signum = sg
     fromInteger x
-      | x < 0     = undefined
-      | x == 0    = undefined
-      | otherwise = undefined
+      | x < 0     = fromInteger (- x)
+      | x == 0    = O
+      | otherwise = S (fromInteger (x - 1))
 

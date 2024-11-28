905fc839f7042ea0b7d532aed3694d0382573668
LucasArruda12 <lucas123manoel4@gmail.com>
2024-11-28 10:10:20 -0300

    ExArithV.hs

--- haskell/ExArithV.hs
+++ haskell/ExArithV.hs
@@ -6,13 +6,21 @@ module ExArithV where
 type Assignment = ()
 
 data ArExV = Atom Integer
+           | Var Char
            | Plus ArExV ArExV
            | Times ArExV ArExV
            | Neg ArExV
 
 -- pretty printer
 pretty :: ArExV -> String
-pretty = undefined
+pretty (Atom x) = show x
+pretty (Var c)  = show c
+pretty (Neg s) = "- " ++ pretty s
+pretty (Plus s t) = "(" ++ pretty s ++ " + " ++ pretty t ++ ")"
+pretty (Times s t) = "(" ++ pretty s ++ " * " ++ pretty t ++ ")"
+ 
+ex1 = (Var 'c') `Plus` (Atom 2)
+ex2 = (Atom 7) `Times` ((Atom 7) `Plus` ((Atom 2) `Times` (Atom 8)))
 
 -- eval evaluates an expression and returns its value
 -- eval :: ?
